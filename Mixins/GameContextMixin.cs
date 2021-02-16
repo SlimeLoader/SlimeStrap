1a82871e37fba7cc8f1497c8413a0ef57f963f35
TehcJS <tehcdev@gmail.com>
2021-02-16 16:39:48 -0500

    i forgot to push

---
+++ Mixins/GameContextMixin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using SharpILMixins.Annotations;
+using SharpILMixins.Annotations.Inject;
+
+namespace SlimeStrap.Mixins {
+	[Mixin("GameContext")]
+	public abstract class GameContextMixin {
+		[Inject(At = AtLocation.Head, Method = "Awake")]
+		private void OnAwake() {
+			// Call "Init" method
+			// TODO: Optimize reflection
+			Type.GetType("SlimeLoader.Loader.ModLoader,Assembly_CSharp")
+				.GetMethod("Init")
+				.Invoke(null, null);
+		}
+	}
+}
