d9d9af0f88c9da19901c2e8b3fbafeb6b2432cb8
TehcJS <tehcdev@gmail.com>
2021-02-18 08:12:19 -0500

    remove assembly reference from mixin and also remove reflection because bad

--- Mixins/GameContextMixin.cs
+++ Mixins/GameContextMixin.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Reflection;
 using SharpILMixins.Annotations;
 using SharpILMixins.Annotations.Inject;
 
 namespace SlimeStrap.Mixins {
-	[Mixin("GameContext,Assembly_CSharp")]
+	[Mixin("GameContext")]
 	public abstract class GameContextMixin {
 		[Inject(At = AtLocation.Head, Method = "Awake")]
 		private void OnAwake() {
 			// Call "Init" method
-			// TODO: Optimize reflection
-			Type.GetType("SlimeLoader.Loader.ModLoader,Assembly_CSharp")
-				.GetMethod("Init")
-				.Invoke(null, null);
 		}
 	}
 }
